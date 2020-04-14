6a26fdbd4a930dd807de9b394fff3c190d9aafdd
HunterEz <61684267+HunterEz@users.noreply.github.com>
2020-04-14 21:09:31 +0500

    Add files via upload

---
+++ 12/12. Очередь(Мальцев).cpp
@@ -0,0 +1,138 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <cstring>
+#include <Windows.h>
+using namespace std;
+
+struct Node
+{
+	char* data;
+	Node* next;
+};
+
+Node* pfirst = NULL, * plast = NULL;
+
+Node* make_point()
+
+{
+
+	Node* p = new(Node);
+
+	p->next = 0;
+
+	char s[50];
+
+	cout << "\n> ";
+
+	cin >> s;
+	p->data = new char[strlen(s) + 1];
+
+	strcpy(p->data, s);
+
+	return p;
+
+}
+
+Node* push(Node*& plast, Node*& pfirst)
+{
+	if (plast == NULL)
+	{
+		plast = make_point();
+		pfirst = plast;
+	}
+	else
+	{
+		Node* temp = make_point();
+		plast->next = temp;
+		plast = temp;
+	}
+	return plast, pfirst;
+}
+
+void createque(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		push(plast, pfirst);
+	}
+
+
+}
+
+void show(Node* plast)
+{
+
+	Node* p = pfirst;
+
+	while (p != 0)
+
+	{
+
+		cout << "\t" << p->data;
+
+		p = p->next;
+
+	}
+
+}
+
+Node* add_str(Node* plast)
+{
+
+	Node* p = pfirst;
+	Node* help;
+	Node* temp = new Node;
+	Node* elem = new Node;
+	cout << "Введите элемент: ";
+	temp = make_point();
+	help = p->next;
+	while (p != 0)
+	{
+		if (!strcmp(p->data, temp->data))
+		{
+			if (help == NULL)
+			{
+				cout << "Введите значение нового элемента: ";
+				Node* temp = make_point();
+				plast->next = temp;
+				plast = temp;
+				return plast;
+			}
+			cout << "Введите значение нового элемента: ";
+			elem = make_point();
+			p->next = elem;
+			elem->next = help;
+			return plast;
+			break;
+
+		}
+
+		p = p->next;
+		if (p == 0)
+		{
+			cout << "Совпадений не обнаружено!" << endl;
+			return plast;
+		}
+		help = p->next;
+	}
+	return plast;
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << "Введите количесатво элементов: ";
+	cin >> n;
+	createque(n);
+	plast = add_str(plast);
+	cout << "Результат :" << endl << ">>";
+	show(plast);
+
+}
+//Тип информационного поля char*. Добавить в список элемент после элемента с заданным информационным полем.
---
+++ 12/12. Стек(Мальцев).cpp
@@ -0,0 +1,130 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <cstring>
+#include <Windows.h>
+using namespace std;
+
+struct Stack
+{
+	char* data;
+	Stack* prev;
+};
+
+
+Stack* plast = NULL;
+
+Stack* make_point()
+
+{
+
+	Stack* p = new(Stack);
+
+	char s[50];
+
+	cout << "\n> ";
+
+	cin >> s;
+	p->data = new char[strlen(s) + 1];
+
+	strcpy(p->data, s);
+
+	return p;
+
+}
+
+
+Stack* push(Stack* plast)
+{
+	Stack* temp;
+	temp = make_point();
+	temp->prev = plast;
+	plast = temp;
+	return plast;
+}
+
+Stack* create_stek(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		plast = push(plast);
+	}
+	return plast;
+}
+
+void pop(Stack* plast)
+{
+	Stack* temp = plast;
+	plast = plast->prev;
+	delete temp;
+}
+
+Stack* add_str(Stack* plast)
+{
+
+	Stack* p = plast;
+	Stack* help;
+	Stack* temp = new Stack;
+	Stack* elem = new Stack;
+	cout << "Введите элемент: ";
+	temp = make_point();
+	help = NULL;
+	while (p != 0)
+	{
+		if (!strcmp(p->data, temp->data))
+		{
+			if (help == NULL)
+			{
+				cout << "Введите значение нового элемента: ";
+				plast = push(plast);
+				return plast;
+			}
+
+			cout << "Введите значение нового элемента: ";
+			elem = make_point();
+			elem->prev = p;
+			help->prev = elem;
+			break;
+			return plast;
+
+		}
+		help = p;
+		p = p->prev;
+	}
+	cout << "Совпадений не обнаружено!" << endl;
+	return plast;
+}
+
+void Show(Stack* plast)
+{
+
+	Stack* temp = plast;
+
+	while (temp != NULL)
+	{
+		cout << endl << ">\t" << temp->data;
+
+		temp = temp->prev;
+
+	}
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n;
+	cout << "Введите количество элементов: ";
+	cin >> n;
+	cout << "Задайте элементы стека" << endl;
+	plast = create_stek(n);
+	plast = add_str(plast);
+	cout << "Стэк элементов:\n";
+	Show(plast);
+}
+
+
+//Тип информационного поля char*. Добавить в стек элемент после элемента с заданным информационным полем.
