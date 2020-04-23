b7bd731fb0efb62c3d43f9c470eef78df546c719
HunterEz <61684267+HunterEz@users.noreply.github.com>
2020-04-23 22:58:30 +0500

    Add files via upload

---
+++ 22/Money.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Money
+{
+	long rub;
+	int cop;
+public:
+	Money() { rub = 0; cop = 0; };
+	Money(long r, int c) { rub = r; cop = c; }
+	Money(const Money& m) { rub = m.rub; cop = m.cop; }
+	~Money() { };
+	long get_rub() { return rub; }
+	int get_cop() { return cop; }
+	void set_rub(long r) { rub = r; }
+	void set_cop(int c) { cop = c;}
+
+	Money& operator=(const Money&);
+	Money& operator--();
+	Money operator--(int);
+	void operator==(const Money&m);
+
+	friend istream& operator>>(istream& in, Money& m);
+	friend ostream& operator<<(ostream& out, const Money& m);
+
+};
+
